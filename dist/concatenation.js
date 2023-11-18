6e6238a94305882764254971d642bfd73af6fc20
zsribna <zsribna@gmail.com>
2023-11-18 22:30:48 +0200

    update

--- dist/concatenation.js
+++ dist/concatenation.js
@@ -1,1 +1,5 @@
-"use strict";
+function concatenation(firstWord, secondWord) {
+    console.log(`${firstWord} ${secondWord}`);
+}
+export { concatenation };
+//# sourceMappingURL=concatenation.js.map
--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,9 @@
-"use strict";
+import { concatenation } from './concatenation';
+const button = document.querySelector('button');
+const input = document.querySelector('input');
+if (button && input) {
+    button.addEventListener('click', () => {
+        concatenation(input.value, 'hello!');
+    });
+}
+//# sourceMappingURL=index.js.map
